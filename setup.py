11b42e9d7862399468944306e7e8d5b241ffa218
Pedro Salgado <steenzout@ymail.com>
2016-10-19 09:26:22 -0600

    use metadata attributes as keyword arguments of the setup function.

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ from setuptools import find_packages, setup
 
 exec(open('steenzout/object/metadata.py').read())
 
-setup(name='steenzout.object',
+setup(name=__package__,
       version=__version__,
-      description='Steenzout Python objects.',
-      author='Pedro Salgado',
-      author_email='steenzout@ymail.com',
-      maintainer='Pedro Salgado',
-      maintainer_email='steenzout@ymail.com',
-      url='https://github.com/steenzout/python-object',
+      description=__description__,
+      author=__author__,
+      author_email=__author_email__,
+      maintainer=__maintainer__,
+      maintainer_email=__maintainer_email__,
+      url=__url__,
       namespace_packages=['steenzout'],
       packages=find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests')),
       package_data={'': ['LICENSE', 'NOTICE.md']},
@@ -26,20 +26,5 @@ setup(name='steenzout.object',
       tests_require=[
           str(pkg.req) for pkg in parse_requirements(
               'requirements-test.txt', session=pip.download.PipSession())],
-
-      license='Apache 2.0',
-      classifiers=(
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'Natural Language :: English',
-          'License :: OSI Approved :: Apache Software License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Programming Language :: Python :: Implementation :: PyPy'
-      ),)
+      license=__license__,
+      classifiers=__classifiers__)
